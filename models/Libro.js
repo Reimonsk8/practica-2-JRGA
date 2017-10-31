357233543882cec7ad279f65b0cef535746fa3b4
Jose Ramón gomez armenta <Reimonsk8@gmail.com>
2017-10-30 20:18:15 -0700

    Add files via upload

---
+++ models/Libro.js
@@ -0,0 +1,9 @@
+var mongoose=require('mongoose');
+var Schema = mongoose.Schema;
+var libroSchema = new Schema({
+	titulo: {type:String},
+	autor: {type: Schema.ObjectId, ref:"Autor", require:true},
+	anio: {type:Number},
+	genero: {type:String}
+});
+module.exports = mongoose.model('Libro',libroSchema);
